f3390e7a2a5d305463ab17cc31881fb061f689f8
diggersdata <105352027+diggersdata@users.noreply.github.com>
2022-05-14 12:14:58 +0530

    with added functionality for CSV tab delimited
    Removed the 1st column from the df, removed the _ (underscore) from PrimaryLeadershipNationality column. Now the script generates 2 files as output. With .xslx & .csv (tab delimited)

---
+++ Detect_Gender_Nationality.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 12:59:48 2022
+
+@author: DATA DIGGERS 
+"""
+
+from names_dataset import NameDataset, NameWrapper
+# The V3 lib takes time to init (the database is massive). Tip: Put it into the init of your app.
+nd = NameDataset()
+
+
+import pandas as pd
+h=pd.read_excel("/Users/ived/Desktop/Python_Gender_Identification/Hackathon_Data_MinorityWomenOwned_2022 v1.xlsx")
+
+"""initial declaration"""
+firstname=[]
+lastname=[]
+gender=[]
+country=[]
+
+"""Firstname """
+for i in range(len(h['executiveContact1'])):
+    firstname.append(str(h['executiveContact1'][i]).split(' ')[0])
+firstname = pd.DataFrame(firstname, columns=['firstname'])
+
+"""LastName"""
+for i in range(len(h['executiveContact1'])):
+    d=str(h['executiveContact1'][i]).split('-')[0]
+    d=d.split(' ')
+    if len(d)>1:
+        if d[len(d)-1]=='':
+            z=d[len(d)-2]
+        else:
+            z=d[len(d)-1]
+        
+    else:
+        z=d[0]
+    lastname.append(z)
+
+lastname = pd.DataFrame(lastname, columns=['lastname'])
+
+
+"""PrimaryLeadershipGender"""
+for i in range(len(firstname)):
+    gender.append(NameWrapper(nd.search(firstname['firstname'][i])).describe.split(',')[0])    
+gender = pd.DataFrame(gender, columns=['PrimaryLeadershipGender'])
+
+h['PrimaryLeadershipGender']=gender['PrimaryLeadershipGender']
+
+
+
+"""PrimaryLeadershipNationality"""
+for i in range(len(lastname)):
+    if len(NameWrapper(nd.search(lastname['lastname'][i])).describe.split(',')[1])>1:
+        country.append(NameWrapper(nd.search(lastname['lastname'][i])).describe.split(',')[1]) 
+    else:
+        country.append(NameWrapper(nd.search(firstname['firstname'][i])).describe.split(',')[1]) 
+ 
+country = pd.DataFrame(country, columns=['PrimaryLeadershipNationality'])
+
+h['PrimaryLeadershipNationality']=country['PrimaryLeadershipNationality']
+final_df=h.iloc[:,1:] # Select From 2nd to end
+
+"""Output excel"""
+final_df.to_excel("Diversity_Owned_Data.xlsx")
+final_df.to_csv('Diversity_Owned_Data_delimited.csv', sep ='\t')
+
+
+
